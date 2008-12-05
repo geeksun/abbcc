155a572bcb63e94e79982cd53ba87c0c30efdae2
jzq9899 <jzq9899@8eb27b8e-c281-11dd-b31d-17274aa020d1>
2008-12-05 09:25:56 +0000

    里面增加了对Hibernate的支持

--- trunk/abbcc/src/com/abbcc/admin/service/UserService.java
+++ trunk/abbcc/src/com/abbcc/admin/service/UserService.java
@@ -5,7 +5,7 @@ import com.abbcc.pojo.Hyjbxx;
 public interface UserService {
 	//public boolean login(Hyjbxx hy);
 
-	public boolean login(String name, String pass);
+	public int login(String name, String pass);
 
 	public int save(Hyjbxx hy) ;      
 	
