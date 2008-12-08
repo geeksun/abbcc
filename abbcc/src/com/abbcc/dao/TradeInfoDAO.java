a38b05f3d2227b9658ace7fe93d3c92f99f9cfb7
jzq9899 <jzq9899@8eb27b8e-c281-11dd-b31d-17274aa020d1>
2008-12-08 15:38:07 +0000

    产品信息DAO

---
+++ abbcc/src/com/abbcc/dao/TradeInfoDAO.java
@@ -0,0 +1,24 @@
+package com.abbcc.dao;
+
+import java.util.List;
+
+import com.abbcc.pojo.TradeInfo;
+
+public interface TradeInfoDAO extends BaseDao {
+	//	 插入会员个人资料
+	public void save(TradeInfo trade);
+	// 修改会员个人资料
+	public void update(TradeInfo trade);
+
+	// 删除会员个人资料
+	public void delete(int tradeInfoId);
+
+	// 根据主键查找会员个人资料
+	public TradeInfo queryById(int tradeInfoId) ;
+
+	public List findByProperty(String name);
+
+	public int findByProperty(String name, String pass);
+
+	//public List getMemberByName(String customer); 
+}
