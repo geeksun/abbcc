1d1a614d811e490f7da5762b12c4b699689d9668
rian2008happy <rian2008happy@8eb27b8e-c281-11dd-b31d-17274aa020d1>
2008-12-09 01:24:53 +0000

    init

---
+++ trunk/abbcc/src/com/abbcc/struts/action/ProductAction.java
@@ -0,0 +1,34 @@
+package com.abbcc.struts.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class ProductAction extends BaseAction {
+	public ActionForward addProduct(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) {
+
+		return mapping.findForward("");
+	}
+
+	public ActionForward productList(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) {
+
+		return mapping.findForward("");
+	}
+
+	public ActionForward deleteProduct(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) {
+
+		return mapping.findForward("");
+	}
+
+	public ActionForward updateProduct(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) {
+
+		return mapping.findForward("");
+	}
+}
