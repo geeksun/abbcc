b4aa84e9ac6c0dc0f04886302fd364c938b50656
rian2008happy <rian2008happy@8eb27b8e-c281-11dd-b31d-17274aa020d1>
2008-12-09 01:24:34 +0000

    init

---
+++ abbcc/src/com/abbcc/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.abbcc.service;
+
+import java.util.List;
+
+import com.abbcc.pojo.Cpgqxx;
+import com.abbcc.pojo.Jytj;
+import com.abbcc.util.Product;
+import com.abbcc.vo.product.bzd.Bzd_csdp;
+
+public interface ProductService {
+	void addProduct(Cpgqxx cpgqxx, Jytj jyty, String type);
+
+	void deleteProduct(int cpgqxxId, int userId);
+
+	void updateProduct(int userId, Cpgqxx cpgqxx, Jytj jyty, String type);
+
+	List<Cpgqxx> productList(int userId);
+
+	Cpgqxx getCpgqxx(  int cpgqxxId);
+
+	Jytj getJytj( int jytjId);
+	
+	Bzd_csdp getBzd_csdp(int bzd_csdpId); 
+	
+	
+	void addProduct(Cpgqxx cpgqxx, Jytj jyty,Product product);
+	
+}
