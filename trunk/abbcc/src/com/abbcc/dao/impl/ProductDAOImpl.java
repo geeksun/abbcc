33f75f76465b2f1ef161865f52e16684c6e1e9e5
rian2008happy <rian2008happy@8eb27b8e-c281-11dd-b31d-17274aa020d1>
2008-12-09 01:34:53 +0000

    init

--- trunk/abbcc/src/com/abbcc/dao/impl/ProductDAOImpl.java
+++ trunk/abbcc/src/com/abbcc/dao/impl/ProductDAOImpl.java
@@ -3,13 +3,13 @@ package com.abbcc.dao.impl;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.abbcc.dao.ProductDAO;
-import com.abbcc.servlet.StattServlet;
-import com.abbcc.util.Product;
+import com.abbcc.servlet.StartServlet;
+import com.abbcc.util.product.Product;
 
 public class ProductDAOImpl implements ProductDAO  {
 
 	public void add(Product product) { 
-		JdbcTemplate jdbc=(JdbcTemplate)StattServlet.getBean("jdbcTemplate");
+		JdbcTemplate jdbc=(JdbcTemplate)StartServlet.getBean("jdbcTemplate");
 		String sql=product.getInsertSql();
 		String[] value=product.getValues();
 		jdbc.update(sql, value);
