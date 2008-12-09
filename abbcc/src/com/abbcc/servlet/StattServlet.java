09e8d9b5b408eefe90c4089b70d770071b59fcce
rian2008happy <rian2008happy@8eb27b8e-c281-11dd-b31d-17274aa020d1>
2008-12-09 01:23:10 +0000

    init

---
+++ abbcc/src/com/abbcc/servlet/StattServlet.java
@@ -0,0 +1,31 @@
+package com.abbcc.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+public class StattServlet extends HttpServlet {
+	private   Log log = LogFactory.getLog(StattServlet.class);
+
+	private static ApplicationContext context;
+
+	public void init(ServletConfig config) {
+		this.initContext(config.getServletContext());
+	}
+
+	private void initContext(ServletContext servlet) {
+
+		context = WebApplicationContextUtils.getWebApplicationContext(servlet);
+
+	}
+
+	public static Object getBean(String name) {
+		 
+		return context.getBean(name);
+	}
+}
