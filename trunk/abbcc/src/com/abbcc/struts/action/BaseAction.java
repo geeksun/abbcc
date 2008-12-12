0597f5ac37747e85be12c4a012e1b17b8f93166e
rian2008happy <rian2008happy@8eb27b8e-c281-11dd-b31d-17274aa020d1>
2008-12-12 04:56:20 +0000

    init  and update prouct

--- trunk/abbcc/src/com/abbcc/struts/action/BaseAction.java
+++ trunk/abbcc/src/com/abbcc/struts/action/BaseAction.java
@@ -2,20 +2,14 @@ package com.abbcc.struts.action;
 
 import org.apache.struts.actions.DispatchAction;
 
-import com.abbcc.factory.DAOFactory;
 import com.abbcc.service.ProductService;
-import com.abbcc.service.ServiceFactory;
+import com.abbcc.servlet.StartServlet;
 
 public class BaseAction extends DispatchAction {
 	
-	protected ProductService productService=ServiceFactory.getProductService();
+	protected ProductService productService= (ProductService) StartServlet.getBean("productService");
+	protected ProductService userService= (ProductService) StartServlet.getBean("userService");
 	
-	private static DAOFactory daofactory;
-	public static DAOFactory getDAOFactory(){
-		if(daofactory==null){
-			daofactory=new DAOFactory();
-		}
-		return daofactory;
-	}
+	 
 
 }
