0afe710a966137a464dcae6504e9ac6676548afc
rian2008happy <rian2008happy@8eb27b8e-c281-11dd-b31d-17274aa020d1>
2008-12-18 13:59:50 +0000

    update add method

--- trunk/abbcc/src/com/abbcc/dao/impl/BaseDaoImpl.java
+++ trunk/abbcc/src/com/abbcc/dao/impl/BaseDaoImpl.java
@@ -1,18 +1,19 @@
 package com.abbcc.dao.impl;
 
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.hibernate.Query;
 import org.springframework.orm.ObjectRetrievalFailureException;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import com.abbcc.dao.BaseDao;
 
 public class BaseDaoImpl extends HibernateDaoSupport implements BaseDao{
-protected final Log log = LogFactory.getLog(getClass());
+	protected   Log log = LogFactory.getLog(getClass());
 	
 	public void delete(Class clazz, Serializable id) {
 		getHibernateTemplate().delete(getObject(clazz, id));		
@@ -34,4 +35,85 @@ protected final Log log = LogFactory.getLog(getClass());
 	public void update(Object o) {
 		getHibernateTemplate().saveOrUpdate(o);
 	}
+   
+
+	public Query getQuery(String querySql, Serializable param) {
+		Query q = this.getSession().createQuery(querySql);
+		q.setParameter(0, param);
+		return q;
+	}
+
+	public Object getObject(String querySql, Serializable param) {
+		return this.getQuery(querySql, param).uniqueResult();
+	}
+
+	public Object getObject(String querySql, Serializable[] param) {
+		return this.getQuery(querySql, param).uniqueResult();
+	}
+
+	public List getList(String querySql ) {
+		return this.getQuery(querySql ).list();
+	}
+	public List getList(String querySql, Serializable param) {
+		return this.getQuery(querySql, param).list();
+	}
+
+	public List getList(String querySql, Serializable[] param) {
+		return this.getQuery(querySql, param).list();
+	}
+
+	public Query getQuery(String querySql, Serializable[] param) {
+		Query query = this.getSession().createQuery(querySql);
+		for (int i = 0; i < param.length; i++) {
+			query.setParameter(i, param[i]);
+		}
+		return query;
+	}
+
+	public Query getQuery(String querySql) {
+		return this.getSession().createQuery(querySql);
+	}
+	/**
+	 * 根据paramters 拼装sql
+	 * @param sql
+	 * @param paramters
+	 * @return
+	 */
+	protected String getSql(String sql ,List paramters){
+		StringBuffer ret=new StringBuffer(sql);
+		int size=paramters.size();
+		if(size==1){
+			ret.append(" =?");
+		}else
+		{
+			ret.append(" in (");
+			Iterator iter=paramters.iterator();
+			int i=0;
+			while(iter.hasNext()){
+				i++;
+				if(i==size)break;
+				ret.append("?,"); 
+				
+			}
+			ret.append("?)");
+		}
+		
+		return ret.toString();
+		
+	}
+	/**
+	 * 设置 查询的 paramters
+	 * @param startIndex 开始设置的位置
+	 * @param query
+	 * @param paramters
+	 */
+	protected void setParamter(int startIndex,Query query,List paramters){
+		Iterator iter=paramters.iterator(); 
+		while(iter.hasNext()){
+			query.setParameter(startIndex, iter.next()); 
+			startIndex++;
+		} 
+	}
+
+
 }
