42718e320b52f1ae176e860ef1e311f88ad9ed0f
rian2008happy <rian2008happy@8eb27b8e-c281-11dd-b31d-17274aa020d1>
2008-12-21 14:17:58 +0000

    delete prouct

--- trunk/abbcc/src/com/abbcc/util/resource/property/FormObject.java
+++
@@ -1,9 +0,0 @@
-package com.abbcc.util.resource.property;
-
-public interface FormObject {
-	public String getKey();
-	public String getName() ;
-	public String getShow();
-	public boolean isNotNull();
-	public boolean isHidden();
-}
